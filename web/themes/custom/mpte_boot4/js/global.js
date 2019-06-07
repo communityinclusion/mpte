3970aa51a44b8ed5372c11269565c2f10b85d073
Paul Foos <paul.foos@umb.edu>
2019-06-07 12:04:47 -0400

    added note above School Districts facet

--- web/themes/custom/mpte_boot4/js/global.js
+++ web/themes/custom/mpte_boot4/js/global.js
@@ -8,7 +8,8 @@
   'use strict';
 
   Drupal.behaviors.mpte_boot4 = {
-    attach: function (context, settings) {
+    attach: function (context, settings) { 
+      if (!$('span.distNote').length) $('#block-schooldistricts h2').after(' <span class="distNote">NOTE: Listed below are only school districts with affiliated interagency groups. Districts not shown have no reported interagency group.<hr /></span>');
 
     }
   };
