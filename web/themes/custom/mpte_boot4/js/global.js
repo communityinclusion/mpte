cff78d24c51b1de3be4b0f91605c20c11f6f8c20
Paul Foos <paul.foos@umb.edu>
2019-07-10 10:12:30 -0400

    edited language above school district search selector

--- web/themes/custom/mpte_boot4/js/global.js
+++ web/themes/custom/mpte_boot4/js/global.js
@@ -9,7 +9,7 @@
 
   Drupal.behaviors.mpte_boot4 = {
     attach: function (context, settings) { 
-      if (!$('span.distNote').length) $('#block-schooldistricts h2').after(' <span class="distNote">NOTE: Listed below are only school districts with affiliated interagency groups. Districts not shown have no reported interagency group.<hr /></span>');
+      if (!$('span.distNote').length) $('#block-schooldistricts h2').after(' <span class="distNote">NOTE: Schools districts listed below participate in one or more of the interagency groups listed on this site. Districts not shown do not participate in any of the groups listed.<hr /></span>');
 
     }
   };
